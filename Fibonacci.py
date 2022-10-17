df3a35d96d94328730a78c4557a507751a067d28
Michael Dasan <59066514+CptnSmash@users.noreply.github.com>
2022-10-17 20:39:43 +1300

    Refactor Fibonnaci.py
    Refactored Fibonacci.py, removing the queue and making use of methods to block out the code

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,37 +1,37 @@
-import queue
 from time import process_time
 
-while True:
-    try:
-        fibNum = int(input("Enter the position of the Fibonacci number you wish to retrieve:\n"))
-        if fibNum <= 0:
-            raise ValueError("Number must be greater than 0")
-        break
-    except ValueError:
-        print("Input could not be resolved to a valid whole number. Please try again.")
 
-fibQ = queue.Queue(2)
-# seed queue with starting values
-fibQ.put(1)
-fibQ.put(1)
+def askNum() -> int:
+    while True:
+        try:
+            fibNum = int(input("Enter the position of the Fibonacci number you wish to calculate:\n"))
+            if fibNum <= 0:
+                raise ValueError("Number must be greater than 0")
+            break
+        except ValueError:
+            print("Input could not be resolved to a valid whole number. Please try again.")
+    return fibNum
 
-start = process_time()
 
-count = 2
-while count < fibNum:
-    # sum the two values in the queue, then place the second value and the new value back into it
-    first = fibQ.get()
-    second = fibQ.get()
-    third = first + second
-    fibQ.put(second)
-    fibQ.put(third)
-    count += 1
+def calculate(fibNum: int):
+    fibOne, fibTwo = 1, 1
 
-# the number sought is now the second in the queue, so remove redundant first element
-fibQ.get()
-print("Fibonacci number #{}: {}".format(fibNum, fibQ.get()))
+    start = process_time()
 
-end = process_time()
-# this may print that the operation completed in 0.0 seconds
-# in that case it means that the operation took less than 0.1 seconds
-print("Operation completed in {} seconds".format(end - start))
+    # count starts at 2 because we already have the first two Fibonacci numbers
+    count = 2
+    while count < fibNum:
+        temp = fibTwo
+        fibTwo += fibOne
+        fibOne = temp
+        count += 1
+
+    print("Fibonacci number #{}: {}".format(fibNum, fibTwo))
+
+    end = process_time()
+    # this may print that the operation completed in 0.0 seconds
+    # in that case it means that the operation took less than 0.1 seconds
+    print("Operation completed in {} seconds".format(end - start))
+
+
+calculate(askNum())
